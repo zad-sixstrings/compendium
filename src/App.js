83d4cb4df6b9aa8747fde4fc02cff806f60fa80f
zad-sixstrings <toxxiczad@gmail.com>
2023-12-21 09:54:53 +0100

    Hello World! This is where it all begins.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import HelloWorld from "./testComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/testComponent.js
@@ -0,0 +1,12 @@
+//src/testComponent.js
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
