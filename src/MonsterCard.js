df25ec0d4d02217ca9a90e319086e2a8434aa80f
zad-sixstrings <toxxiczad@gmail.com>
2023-12-22 09:52:27 +0100

    Successfully integrated expandable description feature

--- src/MonsterCard.js
+++ src/MonsterCard.js
@@ -1,61 +1,55 @@
 // MonsterCard.js
-import React from "react";
+import React, { useState } from "react";
 import mhw_db from "./mhw_db.json";
 import "./MonsterCards.css"; // Import the CSS file
 
+const MonsterCard = ({ monster }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const croppedDescription = isExpanded
+    ? monster.description
+    : `${monster.description.slice(0, 100)}...`;
+
+  return (
+    <div key={monster.id} className="monster-card">
+      <h2>{monster.name}</h2>
+      <p>Species: {monster.species}</p>
+      <p>
+        Description: {croppedDescription}
+        <span className="expand-btn" onClick={toggleDescription}>
+          {isExpanded ? " Less" : "... More"}
+        </span>
+      </p>
+      <p>
+        Weaknesses:{" "}
+        {monster.weaknesses.map((w) => `${w.element} ${w.stars}`).join(", ")}
+      </p>
+      <p>Resistances: {monster.resistances.map((r) => r.element).join(", ")}</p>
+    </div>
+  );
+};
+
 const MonsterCards = ({ searchTerm }) => {
   // Filter large monsters
-  const largeMonsters = mhw_db.filter(
+  const filteredMonsters = mhw_db.filter(
     (monster) =>
       monster.type === "large" &&
       monster.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   // Sort large monsters alphabetically by name
-  const sortedMonsters = largeMonsters.sort((a, b) =>
+  const sortedMonsters = filteredMonsters.sort((a, b) =>
     a.name.localeCompare(b.name)
   );
-
-  // Define the specific order for weaknesses and resistances
-  const order = [
-    "Fire",
-    "Water",
-    "Thunder",
-    "Ice",
-    "Dragon",
-    "Poison",
-    "Sleep",
-    "Paralysis",
-    "Blast",
-    "Stun",
-  ];
 
   return (
     <div className="monster-cards-container">
       {sortedMonsters.map((monster) => (
-        <div key={monster.id} className="monster-card">
-          <h2>{monster.name}</h2>
-          <p>Species: {monster.species}</p>
-          <p>Description: {monster.description}</p>
-          <p>
-            Weaknesses:{" "}
-            {monster.weaknesses
-              .sort(
-                (a, b) => order.indexOf(a.element) - order.indexOf(b.element)
-              )
-              .map((w) => `${w.element} ${w.stars}`)
-              .join(", ")}
-          </p>
-          <p>
-            Resistances:{" "}
-            {monster.resistances
-              .sort(
-                (a, b) => order.indexOf(a.element) - order.indexOf(b.element)
-              )
-              .map((r) => r.element)
-              .join(", ")}
-          </p>
-        </div>
+        <MonsterCard key={monster.id} monster={monster} />
       ))}
     </div>
   );
